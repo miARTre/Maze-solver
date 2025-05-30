f0cd125466bd36c5212a0efcbc29498bfe0d2194
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-05-30 20:52:18 +0200

    Add maze solving methods: solve and _solve_r using depth-first search
    Implemented the recursive depth-first search algorithm in _solve_r to find a path through the maze.
    Added solve method to initiate the recursive solver from the start cell.

--- src/maze.py
+++ src/maze.py
@@ -1,8 +1,9 @@
-import time, random
 from cell import Cell
+import random
+import time
 
 
-class Maze():
+class Maze:
     def __init__(
         self,
         x1,
@@ -11,10 +12,10 @@ class Maze():
         num_cols,
         cell_size_x,
         cell_size_y,
-        win = None,
-        seed = None,
+        win=None,
+        seed=None,
     ):
-    
+        self.__cells = []
         self.__x1 = x1
         self.__y1 = y1
         self.__num_rows = num_rows
@@ -25,13 +26,11 @@ class Maze():
         if seed:
             random.seed(seed)
 
-
-        self.__cells = []
         self.__create_cells()
+        # time.sleep(100)
         self.__break_entrance_and_exit()
         self.__break_walls_r(0, 0)
         self.__reset_cells_visited()
-
 
     def __create_cells(self):
         for i in range(self.__num_cols):
@@ -113,15 +112,76 @@ class Maze():
                 self.__cells[i][j - 1].has_bottom_wall = False
 
             # recursively visit the next cell
-            self.__break_walls_r(next_index[0], next_index[1])    
+            self.__break_walls_r(next_index[0], next_index[1])
 
     def __reset_cells_visited(self):
-        for i in range(self.__num_cols):
-            for j in range(self.__num_rows):
-                self.__cells[i][j].visited = False
+        for col in self.__cells:
+            for cell in col:
+                cell.visited = False
 
+    # returns True if this is the end cell, OR if it leads to the end cell.
+    # returns False if this is a loser cell.
+    def _solve_r(self, i, j):
+        self.__animate()
 
+        # vist the current cell
+        self.__cells[i][j].visited = True
 
+        # if we are at the end cell, we are done!
+        if i == self.__num_cols - 1 and j == self.__num_rows - 1:
+            return True
 
+        # move left if there is no wall and it hasn't been visited
+        if (
+            i > 0
+            and not self.__cells[i][j].has_left_wall
+            and not self.__cells[i - 1][j].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i - 1][j], True)
 
-                
+        # move right if there is no wall and it hasn't been visited
+        if (
+            i < self.__num_cols - 1
+            and not self.__cells[i][j].has_right_wall
+            and not self.__cells[i + 1][j].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i + 1][j], True)
+
+        # move up if there is no wall and it hasn't been visited
+        if (
+            j > 0
+            and not self.__cells[i][j].has_top_wall
+            and not self.__cells[i][j - 1].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j - 1], True)
+
+        # move down if there is no wall and it hasn't been visited
+        if (
+            j < self.__num_rows - 1
+            and not self.__cells[i][j].has_bottom_wall
+            and not self.__cells[i][j + 1].visited
+        ):
+            self.__cells[i][j].draw_move(self.__cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j + 1], True)
+
+        # we went the wrong way let the previous cell know by returning False
+        return False
+
+    # create the moves for the solution using a depth first search
+    def solve(self):
+        return self._solve_r(0, 0)
